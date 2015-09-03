c1ebfbfb3d76c4b819b8a95613d389f1ae11a6ab
jharnold <jharnold@fullsail.edu>
2015-09-02 23:27:45 -0400

    Expressions Worksheet v4
    Expressions Worksheet v4
    
    ADDED:
    
    - prompt to inform user of the programs purpose

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -4,7 +4,7 @@ SDI
 Expressions Worksheet - Area of a Circle
 09/02/2015
  */
-
+alert("Today we are going to find the area of a circle.");
 var usrRadius = prompt("What is the radius of our circle?");
 const pI = 3.14159265;
 area = usrRadius * usrRadius * pI;
