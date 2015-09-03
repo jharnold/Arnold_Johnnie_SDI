af718b1f06c9e9008f01344d746aec8638fd1e74
jharnold <jharnold@fullsail.edu>
2015-09-03 00:07:13 -0400

    Dog Years - FINAL, FINAL
    Dog Years - FINAL, FINAL
    
    ADDED:
    
    -text to first prompt to inform user of the programs purpose

--- Expressions_Worksheet/js/sparky.js
+++ Expressions_Worksheet/js/sparky.js
@@ -5,7 +5,7 @@ Expressions Worksheet - Dog Years
 09/02/2015
  */
 
-alert("It is said that dogs age 7 times faster than humans."); //alert about dogs aging 7 times faster than humans
+alert("It is said that dogs age 7 times faster than humans.\nLet's see what that really means..."); //alert about dogs aging 7 times faster than humans and alerts user of programs purpose
 var sparkyAge = prompt("Today we are going to find out how old sparky is in dog years.\n\nHow old is sparky in human years?"); //gathers user input of age
 dogYears = 7; //dogYears = 7
 var sparkyMath = sparkyAge * dogYears; //user's number times 7
