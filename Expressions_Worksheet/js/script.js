debc0826f1f3a7e5716b9d8f9c67fe3035edd97b
jharnold <jharnold@fullsail.edu>
2015-09-02 23:20:58 -0400

    Expressions Worksheet v1
    Area of a Circle

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+/*
+Johnnie Arnold
+SDI
+Expressions Worksheet - Area of a Circle
+09/02/2015
+ */
+
+var usrRadius = prompt("What is the radius of our circle?");
+var pI = 3.14;
+area = usrRadius * usrRadius * pI;
+
+console.log("The area of your circle is " + area);
+
