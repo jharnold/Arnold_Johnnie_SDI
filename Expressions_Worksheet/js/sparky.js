e61bdafc8476b5e347be1b6b0cfd8e8d6ac087ec
jharnold <jharnold@fullsail.edu>
2015-09-02 23:48:42 -0400

    Expressions Worksheet - Dog Years v1
    Expressions Worksheet - Dog Years v1

---
+++ Expressions_Worksheet/js/sparky.js
@@ -0,0 +1,12 @@
+/*
+Johnnie Arnold
+SDI
+Expressions Worksheet - Dog Years
+09/02/2015
+ */
+
+var sparkyAge = prompt("Today we are going to find out how old sparky is in dog years.\n\nHow old is sparky in human years?");
+dogYears = 7;
+var sparkyMath = sparkyAge * dogYears;
+
+console.log("Sparky is " + sparkyAge + " human years old which is " + sparkyMath + " in dog years.");
