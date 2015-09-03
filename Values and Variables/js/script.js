9d79320a624e35d37177103503855fdd8cf814d9
jharnold <jharnold@fullsail.edu>
2015-09-02 22:21:49 -0400

    Concatenating
    Concatenating

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,7 +1,7 @@
 //JavaScript
 
-var orangeBins = [234, 567, 883];
+var firstName = "Johnnie";
+var lastName = "Arnold";
+var fullName = firstName + " " + lastName;
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-
-console.log(total);
+console.log(fullName);
