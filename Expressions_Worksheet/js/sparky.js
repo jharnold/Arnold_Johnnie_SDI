ed47a74681d7b1ccf2c06481208320c04dc9e32c
jharnold <jharnold@fullsail.edu>
2015-09-03 00:04:11 -0400

    Expressions Worksheet - Dog Years - Final
    Dog Years - Final
    
    FIXED:
    
    - Some lines didn’t have “;”
    
    ADDED:
    
    - extra alerts and line explanations

--- Expressions_Worksheet/js/sparky.js
+++ Expressions_Worksheet/js/sparky.js
@@ -5,10 +5,10 @@ Expressions Worksheet - Dog Years
 09/02/2015
  */
 
-alert("It is said that dogs age 7 times faster than humans.")
-var sparkyAge = prompt("Today we are going to find out how old sparky is in dog years.\n\nHow old is sparky in human years?");
-dogYears = 7;
-var sparkyMath = sparkyAge * dogYears;
+alert("It is said that dogs age 7 times faster than humans."); //alert about dogs aging 7 times faster than humans
+var sparkyAge = prompt("Today we are going to find out how old sparky is in dog years.\n\nHow old is sparky in human years?"); //gathers user input of age
+dogYears = 7; //dogYears = 7
+var sparkyMath = sparkyAge * dogYears; //user's number times 7
 
-alert("To find out how old sparky is in dog years please check the browser's console.")
-console.log("Sparky is " + sparkyAge + " human years old which is " + sparkyMath + " in dog years.");
+alert("To find out how old sparky is in dog years please check the browser's console."); //alerts user to view the browser's console for the answer
+console.log("Sparky is " + sparkyAge + " human years old which is " + sparkyMath + " in dog years."); //prints result to browser's console
