1910f301dc4d0254be9a7c3f65440dc494a8f15d
jharnold <jharnold@fullsail.edu>
2015-09-02 22:42:27 -0400

    Assignment Op
    Assignment Operators

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,9 +1,7 @@
 //JavaScript
 
-var areaCode = 678;
-var firstPart = 759;
-var secondPart = 8217;
-//
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secondPart);
-console.log(phoneNo);
+var a = 3;
+a ++; //a += 1
+console.log(a);
+
 
