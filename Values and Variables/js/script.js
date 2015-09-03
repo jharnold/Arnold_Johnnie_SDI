8a353d319021bfd13521b85ff3d5d7330fc66b37
jharnold <jharnold@fullsail.edu>
2015-09-02 22:16:25 -0400

    Expressions With Arrays
    Expressions with Arrays

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,19 +1,7 @@
 //JavaScript
 
-//PEMDAS
+var orangeBins = [234, 567, 883];
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
 
-//the average is the sum of the items divided by the number of items
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-//console.log(average);
-
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2;
-
-console.log(perimeter);
+console.log(total);
