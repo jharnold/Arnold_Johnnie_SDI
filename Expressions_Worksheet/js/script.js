fc45767bdd2e828482ef565b92f3e793e8748dc5
jharnold <jharnold@fullsail.edu>
2015-09-02 23:34:26 -0400

    Expressions Worksheet v5 - Area of a Circle
    Expressions Worksheet v5 - Area of a Circle
    
    ADDED:
    
    - line information
    - alert to tell the user to check the browser’s console for the answer
    - added “ sq feet” to the answer

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -4,10 +4,11 @@ SDI
 Expressions Worksheet - Area of a Circle
 09/02/2015
  */
-alert("Today we are going to find the area of a circle.");
-var usrRadius = prompt("What is the radius of our circle?");
-const pI = 3.14159265;
-area = usrRadius * usrRadius * pI;
+alert("Today we are going to find the area of a circle."); //alerts the user to the purpose of the program
+var usrRadius = prompt("What is the radius of our circle?"); //prompts for the user to give a number
+const pI = 3.14159265; //pI = 3.14159265
+area = usrRadius * usrRadius * pI; //calculates the area with the users input
 
-console.log("The area of your circle is " + area);
+alert("Please use the browser's console to view the answer.") //alerts the user to view the browser's console
+console.log("The area of your circle is " + area + " sq feet!"); //prints the answer in the console
 
