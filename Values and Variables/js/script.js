52274b59bf8742462ededd9a02da0124a4fc9f78
jharnold <jharnold@fullsail.edu>
2015-09-02 22:00:48 -0400

    Arithmetic Operators
    Arithmetic Operators

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -2,8 +2,8 @@
 
 //area of triangle is half of the width times height
 
-var width = 4;
-var height = 5;
-
-var area = width * height;
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
 
