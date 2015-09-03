d5352bf3f53837e0645761a7fb07a61766abab19
jharnold <jharnold@fullsail.edu>
2015-09-02 23:52:41 -0400

    Expressions Worksheet - Dog Years v2
    Dog Years v2
    
    ADDED:
    
    - alert that states “It is said that dogs age 7 times faster than
    humans.”

--- Expressions_Worksheet/js/sparky.js
+++ Expressions_Worksheet/js/sparky.js
@@ -5,6 +5,7 @@ Expressions Worksheet - Dog Years
 09/02/2015
  */
 
+alert("It is said that dogs age 7 times faster than humans.")
 var sparkyAge = prompt("Today we are going to find out how old sparky is in dog years.\n\nHow old is sparky in human years?");
 dogYears = 7;
 var sparkyMath = sparkyAge * dogYears;
