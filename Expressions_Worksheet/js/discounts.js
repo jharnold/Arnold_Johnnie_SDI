2d5bc23f17a449d440fef325fe61602e038f9082
jharnold <jharnold@fullsail.edu>
2015-09-07 00:21:57 -0400

    Discount
    Discount

---
+++ Expressions_Worksheet/js/discounts.js
@@ -0,0 +1,19 @@
+/*
+Johnnie Arnold
+Expressions Worksheet - Discounts
+ */
+var item = prompt("What would you like to buy?");
+var price = prompt("How much is does the " + item + "cost?");
+var discount = prompt("How much is your discount?\nPlease make this a decimal (Example: .20 = 20%:)");
+var tax = prompt("How much tax is charged?\nPlease make this a decimal (Example: .20 = 20%:)");
+
+//discount calculation
+var calcDiscount = price * discount ;
+var calcPrice = calcDiscount - price;
+
+//tax calculation
+var wTax = calcPrice * tax;
+var taxPrice = wTax - calcPrice;
+
+
+
