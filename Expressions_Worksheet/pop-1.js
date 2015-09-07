424c98f6ec0bb43e2fdcd1141e666cf23564b1bc
jharnold <jharnold@fullsail.edu>
2015-09-06 21:01:12 -0400

    Piece of Pie Part 1
    Piece of Pie Part 1

---
+++ Expressions_Worksheet/pop-1.js
@@ -0,0 +1,10 @@
+/*
+Johnnie Arnold
+SDI
+Expressions Worksheet - Piece of Pie Part 1
+ */
+
+alert("Welcome to the party!\nLet's find out how many pieces of pizza everyone ate.');
+var numPeeps = prompt("How many people are at the party?");
+var numPizzas = prompt("How many pizzas were ordered?");
+var numSlices = prompt("How may slices are there for each pizza?");
