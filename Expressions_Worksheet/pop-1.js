9b38c7ce90d5a4419bb64db5cefd14d319ad61bc
jharnold <jharnold@fullsail.edu>
2015-09-06 21:07:26 -0400

    Slice of Pie Part 1
    Slice of Pie Part 1

--- Expressions_Worksheet/pop-1.js
+++ Expressions_Worksheet/pop-1.js
@@ -1,10 +1,12 @@
 /*
 Johnnie Arnold
-SDI
 Expressions Worksheet - Piece of Pie Part 1
  */
 
-alert("Welcome to the party!\nLet's find out how many pieces of pizza everyone ate.');
+alert("Let's find out how many pieces of pizza everyone ate at the party!");
 var numPeeps = prompt("How many people are at the party?");
 var numPizzas = prompt("How many pizzas were ordered?");
 var numSlices = prompt("How may slices are there for each pizza?");
+
+var sliceCalc = numPizzas * numSlices/numPeeps;
+
