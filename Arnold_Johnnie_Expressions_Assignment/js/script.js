a57e0be001615c28785f4d191fad2b91069314ef
jharnold <jharnold@fullsail.edu>
2015-09-07 02:36:34 -0400

    Expressions Assignment
    Expressions Assignment

--- Arnold_Johnnie_Expressions_Assignment/js/script.js
+++ Arnold_Johnnie_Expressions_Assignment/js/script.js
@@ -8,15 +8,15 @@ alert("Hello " + userName + ". It seems that you have an account with us!");
 var aDeps = [1000, 250, 6000, 10000];
 var aWits = [900, 100, 3000];
 var accountD = aDeps[0] + aDeps[1] + aDeps[2] + aDeps[3];
-var accountW = accountD - (aWits[0] + aWits[1] + aWits[2]);
+var accountW = accountD -= (aWits[0] + aWits[1] + aWits[2]);
+var plus1 = aDeps[3] + 1;
 
 
 alert("You recently made these deposit(s) of: $" + aDeps[0] + ", $" + aDeps [1] + ", $" + aDeps[2] + ", $" + aDeps[3] + ".");
 alert("You recently made these withdrawal(s) of: $" + aWits[0] + ", $"  + aWits[1] + ", $"  + aWits[2] + ".");
 alert("This puts your grand total at: $" + accountW);
 
-var creditIt = aDeps[3]++;
-alert(userName + ", we at Faking Banking would like to show our appreciation for YOU by crediting your account with $1.00 for the largest deposit you've made! This credit makes your largest deposit of $" + creditIt + " into $" + aDeps[3]);
+alert(userName + ", we at Faking Banking would like to show our appreciation for YOU by crediting your account with $1.00 for the largest deposit you've made! This credit makes your largest deposit of $" + aDeps[3] + " into $" + plus1 + ".");
 
 prompt("HOLD ON! We're not finished yet.");
 
@@ -24,7 +24,7 @@ userDeps = prompt("You need to put MORE money into your account! How much would 
 
 alert("You seriously want to put $" + userDeps + " into your account? Well, okay then.");
 
-var userFinal = userDeps + accountW++ ;
+var userFinal = userDeps + accountW +  1;
 
 alert(userName + ", your account now holds... Oh, hold on. You have to check your browser's console for the answer.");
 
