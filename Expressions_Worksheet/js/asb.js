e2ca82d596921a2bfa26f6d4cb691d7054ba4d4e
jharnold <jharnold@fullsail.edu>
2015-09-06 23:59:44 -0400

    Avg Shopping Bill
    Avg Shopping Bill - FINAL - I couldn’t find a way use the user’s input
    from prompt to set the array so I hardcoded it

--- Expressions_Worksheet/js/asb.js
+++ Expressions_Worksheet/js/asb.js
@@ -5,8 +5,8 @@ Expressions Worksheet
 09/06/2015
  */
 
-var gBills = [123, 456, 789, 1011, 1213];
-var total = gBills[0] + gBills[1] + gBills[2] + gBills[3] + gBills[4];
-var average = (gBills[0] + gBills[1] + gBills[2] + gBills[3] + gBills[4])/5;
+var gBills = [123, 456, 789, 1011, 1213]; //array of 5 weeks of grocery bills
+var total = gBills[0] + gBills[1] + gBills[2] + gBills[3] + gBills[4]; //bills added together from array
+var average = (gBills[0] + gBills[1] + gBills[2] + gBills[3] + gBills[4])/5; //bills added together from array divided by 5 weeks
 
-alert("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average +" each week.")
+console.log("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average +" each week.");
