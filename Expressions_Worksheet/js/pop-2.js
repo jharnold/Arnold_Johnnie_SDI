364d8238db1a17b54ca423e25de0ae8a71d15fb0
jharnold <jharnold@fullsail.edu>
2015-09-06 21:47:57 -0400

    Slice of Pie 2
    Slice of Pie 2

---
+++ Expressions_Worksheet/js/pop-2.js
@@ -0,0 +1,15 @@
+/*
+Johnnie Arnold
+SDI
+Expressions Worksheet - Slice of Pie Part 2
+09/06/2015
+ */
+alert("Let's find out how many pieces of pizza did Sparky got...");
+
+var numPeeps = prompt("How many people are at the party?"); //user input for amount of people at the party
+var numPizzas = prompt("How many pizzas were ordered?"); //user input for amount of pizzas ordered
+var numSlices = prompt("How may slices are there for each pizza?"); //user input for the amount of slices per pizza
+
+var sliceCalc = numPizzas * numSlices/numPeeps;
+var sparkySlices = sliceCalc/numPizzas;
+
