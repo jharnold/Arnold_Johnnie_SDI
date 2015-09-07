0ffdf7f3e3a901c989796bd48bb258c4edb02991
jharnold <jharnold@fullsail.edu>
2015-09-07 00:44:39 -0400

    Discount
    Discount

--- Expressions_Worksheet/js/discounts.js
+++ Expressions_Worksheet/js/discounts.js
@@ -8,15 +8,20 @@ var discount = prompt("How much is your discount?\n(Example: 25 for 25%)");
 var tax = prompt("How much tax is charged?\n (Example 7 for 7%)");
 
 //discount calculation
-var calcDiscount = price * discount ;
-var calcPrice = calcDiscount - price;
+disPer = discount/100;
+var calcDiscount = price * disPer ;
+var calcPrice = price - calcDiscount;
+
 
 //tax calculation
-var wTax = calcPrice * tax;
-var taxPrice = wTax - calcPrice;
+taxPer = tax/100;
+var wTax = calcPrice * taxPer;
+var taxPrice = calcPrice + tax;
 
 //Decimal to Whole Calculation
-var decWhole = discount/100;
+var disWhole = discount/100;
+var texPerc = tax/100;
 
-alert("Your " + item + " was originally $" + price + ", but after a " + decWhole + "% discount, it is now $" + calcPrice + " without tax, and $" + taxPrice + " with tax." );
+
+alert("Your " + item + " was originally $" + price + ", but after a " + discount + "% discount, it is now $" + calcPrice + " without tax, and $" + taxPrice + " with tax." );
 
