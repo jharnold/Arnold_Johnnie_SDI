5805509a037ea6cd2736498adaf1130139a996e1
jharnold <jharnold@fullsail.edu>
2015-09-07 02:23:01 -0400

    Expressions Assignment
    Expressions Assignment

--- Arnold_Johnnie_Expressions_Assignment/js/script.js
+++ Arnold_Johnnie_Expressions_Assignment/js/script.js
@@ -15,9 +15,8 @@ alert("You recently made these deposit(s) of: $" + aDeps[0] + ", $" + aDeps [1] 
 alert("You recently made these withdrawal(s) of: $" + aWits[0] + ", $"  + aWits[1] + ", $"  + aWits[2] + ".");
 alert("This puts your grand total at: $" + accountW);
 
-var creditAdd = aDeps[3]++;
-
-var creditAlert = alert(userName + ", we at Faking Banking would like to show our appreciation for YOU by crediting your account with $1.00 for the largest deposit you've made! This credit makes your largest deposit of $" + aDeps[3] + "into $" + creditAdd + ".");
+var creditIt = aDeps[3]++;
+alert(userName + ", we at Faking Banking would like to show our appreciation for YOU by crediting your account with $1.00 for the largest deposit you've made! This credit makes your largest deposit of $" + creditIt + " into $" + aDeps[3]);
 
 prompt("HOLD ON! We're not finished yet.");
 
@@ -25,7 +24,7 @@ userDeps = prompt("You need to put MORE money into your account! How much would 
 
 alert("You seriously want to put $" + userDeps + " into your account? Well, okay then.");
 
-var userFinal = userDeps + accountW + creditAdd;
+var userFinal = userDeps + accountW++ ;
 
 alert(userName + ", your account now holds... Oh, hold on. You have to check your browser's console for the answer.");
 
