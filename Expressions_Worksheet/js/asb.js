f8030a83f1cb7c7221eb9e44b33ad9f347985cf1
jharnold <jharnold@fullsail.edu>
2015-09-06 23:44:20 -0400

    Avg Shopping Bill
    Avg Shopping Bill

---
+++ Expressions_Worksheet/js/asb.js
@@ -0,0 +1,14 @@
+/*
+Johnnie Arnold
+Expressions Worksheet
+ */
+
+var userInput = prompt("Please enter your past 5 grocery bills:");
+bill1 = userInput[0];
+bill2 = userInput[1];
+
+billTotal = bill1[0] + bill2[1];
+
+alert(billTotal);
+
+
