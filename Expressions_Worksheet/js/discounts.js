2ec42e48544e862566d2cb5825353dc3f5a42016
jharnold <jharnold@fullsail.edu>
2015-09-07 00:54:08 -0400

    Discount
    Discount

--- Expressions_Worksheet/js/discounts.js
+++ Expressions_Worksheet/js/discounts.js
@@ -2,26 +2,22 @@
 Johnnie Arnold
 Expressions Worksheet - Discounts
  */
-var item = prompt("What would you like to buy?");
-var price = prompt("How much is does the " + item + " cost?");
-var discount = prompt("How much is your discount?\n(Example: 25 for 25%)");
-var tax = prompt("How much tax is charged?\n (Example 7 for 7%)");
+var item = prompt("What would you like to buy?"); //user inputs the item name
+var price = prompt("How much is does the " + item + " cost?"); //asks user the price of item
+var discount = prompt("How much is your discount?\n(Example: 25 = 25%)"); //user input for discount
+var tax = prompt("How much tax is charged?\n (Example 7 = 7%)"); //user input for tax
 
 //discount calculation
-disPer = discount/100;
-var calcDiscount = price * disPer ;
-var calcPrice = price - calcDiscount;
+disPer = discount/100; //changes discount into a decimal
+var calcDiscount = price * disPer; //price * decimal discount amount
+var calcPrice = price - calcDiscount; //user price - calcDiscount
 
 
 //tax calculation
-taxPer = tax/100;
-var wTax = calcPrice * taxPer;
-var taxPrice = calcPrice + tax;
-
-//Decimal to Whole Calculation
-var disWhole = discount/100;
-var texPerc = tax/100;
+taxPer = tax/100; //turns tax amount into decimal
+var wTax = calcPrice * taxPer; //calcPrice * decimal tax amount
+var taxPrice = calcPrice + wTax; //calcPrice - wtax
 
 
-alert("Your " + item + " was originally $" + price + ", but after a " + discount + "% discount, it is now $" + calcPrice + " without tax, and $" + taxPrice + " with tax." );
+console.log("Your " + item + " was originally $" + price + ", but after a " + discount + "% discount, it is now $" + calcPrice + " without tax, and $" + taxPrice + " with tax." ); //results
 
