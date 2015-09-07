6b9936dbfcfed50f4768492b199dc6c552919fd4
jharnold <jharnold@fullsail.edu>
2015-09-06 23:54:38 -0400

    Avg Shopping Bill
    Avg Shopping Bill

--- Expressions_Worksheet/js/asb.js
+++ Expressions_Worksheet/js/asb.js
@@ -7,3 +7,4 @@ var gBills = [111, 222, 333, 444, 555];
 var total = gBills[0] + gBills[1] + gBills[2] + gBills[3] + gBills[4];
 var average = (gBills[0] + gBills[1] + gBills[2] + gBills[3] + gBills[4])/5;
 
+alert("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average +" each week.")
