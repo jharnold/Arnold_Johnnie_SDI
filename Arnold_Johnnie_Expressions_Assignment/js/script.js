2ff163255451ff857236229729cf6caeaa7663a7
jharnold <jharnold@fullsail.edu>
2015-09-07 01:17:13 -0400

    Expressions Assignment
    Expressions Assignment

---
+++ Arnold_Johnnie_Expressions_Assignment/js/script.js
@@ -0,0 +1,10 @@
+/*
+Johnnie Arnold
+Expressions Assignment
+09/07/2015
+ */
+var userName = prompt("Welcome to Faking Banking.\nPlease enter your name:");
+var hello = prompt("Hello " + userName + ".\n It seems that you have an account with us.");
+var aDeps = [1000, 250, 6000, 100000];
+var aWits = [900, 100, 3000, 500000];
+var accountNfo = aDeps[0] + aDeps[1] + aDeps[2] + aDeps[3];
