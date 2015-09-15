b66351be10840c41203143a76116d67a97448564
jharnold <jharnold@fullsail.edu>
2015-09-15 18:53:06 -0500

    conditionals
    conditionals

--- ScreenCasts/js/script.js
+++ ScreenCasts/js/script.js
@@ -1,13 +1,13 @@
 //Conditional Logic
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48;
 
 //if child is old enough print to console "You can ride!"
-
-if(oldEnough){
+//if kid is over 48 inches in height
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-
 }
-console.log("What comes after");
+
 
