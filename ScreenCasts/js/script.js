0c79242149abfa58e1d7b9b741e66e14bacf8879
jharnold <jharnold@fullsail.edu>
2015-09-15 19:07:22 -0500

    conditionals
    conditionals

--- ScreenCasts/js/script.js
+++ ScreenCasts/js/script.js
@@ -2,13 +2,17 @@
 
 var kidHeight = 52;
 var minHeight = 48;
-
+var parentHeight = 45;
 
 //if child is old enough print to console "You can ride!"
 //if kid is over 48 inches in height
 if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+
+}else if(kidHeight > parentHeight){
+    console.log("You can ride with a parent");
 } else {
+    //you can ride with a parent
     console.log("Sorry kiddo");
 }
