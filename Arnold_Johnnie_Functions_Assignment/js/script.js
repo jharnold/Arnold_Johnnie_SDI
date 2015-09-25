2c5ab111279480a9203e78597330d9258e21f118
jharnold <jharnold@fullsail.edu>
2015-09-24 21:01:05 -0500

    Functions Assignment
    Functions Assignment

--- Arnold_Johnnie_Functions_Assignment/js/script.js
+++ Arnold_Johnnie_Functions_Assignment/js/script.js
@@ -4,42 +4,30 @@ SDI
 Functions Assignment
 09/24/2015
 */
+
+var acctAmt = 1000.00;
 var userName = prompt("Please enter your name:");
 userName = userName.toLowerCase();
+while(userName ==="") {
+    alert("Please don't leave your name field blank:");
+    prompt(userName);
+}
+
+if(userName === isNaN){
+    alert(userInp);
+}
+
 var userInp = prompt("Welcome to Faking Banking ATM.\nWould you to Check your (B)alance or (W)ithdrawl cash?");
 userInp = userInp.toLowerCase();
-var acctAmt = 1000.00;
-
-while(userName===""){
-    if(userName===""){
-        prompt("Please don't leave your name field blank:");
-    }else if(userName=== isNaN){
-        alert("Thank you "+userName+".");
+while(userInp != "b" && userInp != "w" || userInp==="") {
+    if (userInp === "b") {
+        acctBal();
     }
-}
-
-while(userInp===""){
-    if(userInp===""){
-        prompt("Please do not leave this field blank.\n(B)alance or (W)ithdrawal:");
+    if (userInp === "w") {
+        acctWith();
     }
-}
-while(userInp != "b" && userInp != "w"){
-    if(userInp != "b" && userInp != "w"){
-        prompt("Please use B for balance or W for withdrawal:")
-    }
-}
 
-function acctBal() {
-    alert("Your balance is $" + acctAmt+".");
-    alert("Thank you banking with Faking Banking.");
-}
 
-function acctWith(){
-    var amountWith = Number(prompt("Your account is $"+acctAmt+". How much would you like to withdrawal?"));
-    while(isNaN(amountWith)){
-        Number(prompt("Please enter a numerical amount:"));
-    }
-    var remainingBal = acctAmt - amountWith;
-    var final = alert("You withdrew $"+amountWith+". Your remaining balance is $");
-}
+
+
 
