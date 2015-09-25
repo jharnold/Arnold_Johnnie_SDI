e175eff8f709f1a654b00b5ca6aa2561ad4b8cb2
jharnold <jharnold@fullsail.edu>
2015-09-24 20:10:55 -0500

    functions assignment
    functions assignment

--- Arnold_Johnnie_Functions_Assignment/js/script.js
+++ Arnold_Johnnie_Functions_Assignment/js/script.js
@@ -4,10 +4,19 @@ SDI
 Functions Assignment
 09/24/2015
 */
-
+var userName = prompt("Please enter your name:");
+userName = userName.toLowerCase();
 var userInp = prompt("Welcome to Faking Banking ATM.\nWould you to Check your (B)alance or (W)ithdrawl cash?");
 userInp = userInp.toLowerCase();
 var acctAmt = 1000.00;
+
+while(userName===""){
+    if(userName===""){
+        prompt("Please don't leave your name field blank:");
+    }else if(userName=== isNaN){
+        alert("Thank you "+userName+".");
+    }
+}
 
 while(userInp===""){
     if(userInp===""){
@@ -26,8 +35,11 @@ function acctBal() {
 }
 
 function acctWith(){
-    var amountWith = prompt("Your account is $"+acctAmt+". How much would you like to withdrawal?");
-    var remainingBal = acctAmt - amountWith
-    var final = alert("You withdrew $"+amountWith+". Your remaining balance is $")
+    var amountWith = Number(prompt("Your account is $"+acctAmt+". How much would you like to withdrawal?"));
+    while(isNaN(amountWith)){
+        Number(prompt("Please enter a numerical amount:"));
+    }
+    var remainingBal = acctAmt - amountWith;
+    var final = alert("You withdrew $"+amountWith+". Your remaining balance is $");
 }
 
