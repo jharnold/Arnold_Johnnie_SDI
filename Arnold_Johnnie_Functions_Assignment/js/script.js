2398419b2dacf76601b687e34d507458ce5b82ca
jharnold <jharnold@fullsail.edu>
2015-09-24 19:59:04 -0500

    Function Assignment
    Functions assignment

--- Arnold_Johnnie_Functions_Assignment/js/script.js
+++ Arnold_Johnnie_Functions_Assignment/js/script.js
@@ -5,18 +5,29 @@ Functions Assignment
 09/24/2015
 */
 
-var userInp = prompt("Welcome to Faking Banking ATM. Would you to Check your (B)alance or (W)ithdrawl cash?");
-
+var userInp = prompt("Welcome to Faking Banking ATM.\nWould you to Check your (B)alance or (W)ithdrawl cash?");
 userInp = userInp.toLowerCase();
+var acctAmt = 1000.00;
 
-while(userInp !="b" && userInp !="w") {
-    prompt("I'm sorry, but that input is invalid.\nPlease enter B for Balance or W for withdrawal.");
-}
-
-function userAcct(balance, withdrawl){
-    if(userInp = "b"){
-        balanceIt = Math.round(Math.random()*100);
-        alert("Your balance is: $"+balanceIt);
+while(userInp===""){
+    if(userInp===""){
+        prompt("Please do not leave this field blank.\n(B)alance or (W)ithdrawal:");
     }
-
 }
+while(userInp != "b" && userInp != "w"){
+    if(userInp != "b" && userInp != "w"){
+        prompt("Please use B for balance or W for withdrawal:")
+    }
+}
+
+function acctBal() {
+    alert("Your balance is $" + acctAmt+".");
+    alert("Thank you banking with Faking Banking.");
+}
+
+function acctWith(){
+    var amountWith = prompt("Your account is $"+acctAmt+". How much would you like to withdrawal?");
+    var remainingBal = acctAmt - amountWith
+    var final = alert("You withdrew $"+amountWith+". Your remaining balance is $")
+}
+
